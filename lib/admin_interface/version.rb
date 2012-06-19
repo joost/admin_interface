77be8ee131a957c6cfa82af2311b7554cf595763
Joost Hietbrink <joost@joopp.com>
2012-06-19 15:20:36 +0200

    Fix missing wrong translations nasty way.

--- lib/admin_interface/version.rb
+++ lib/admin_interface/version.rb
@@ -2,8 +2,8 @@
 module AdminInterface
   module Version
     MAJOR = 2
-    MINOR = 1
-    PATCH = 15
+    MINOR = 2
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
