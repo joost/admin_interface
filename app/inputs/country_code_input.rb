c11d6a7931a1133972d97832c2ebbfbfedc53a80
Joost Hietbrink <joost@joopp.com>
2013-02-12 14:50:50 +0100

    Fix for SimpleForm country code select.

--- app/inputs/country_code_input.rb
+++ app/inputs/country_code_input.rb
@@ -3,8 +3,16 @@
 # Usage (in your simple_form):
 #   <%= form.input :country_code, :as => :country_code %> 
 #
+# In your SimpleForm config add:
+#   config.country_priority = ['', 'NL', 'BE', 'DE', 'FR', 'IT', 'GB', 'US', 'AU']
+#
 class CountryCodeInput < SimpleForm::Inputs::Base
   def input
-    @builder.country_code_select(attribute_name, input_options.delete(:priority) || SimpleForm.country_priority, input_html_options) #, priority_countries, options)
+    priority_countries = input_options.delete(:priority) || SimpleForm.country_priority.clone
+    if not priority_countries.first.is_a?(Array)
+      # We allow setting priority_countries by using only code..
+      priority_countries.collect! { |code| [I18n.t(code.to_sym, :scope => :countries), code]}
+    end
+    @builder.country_code_select(attribute_name, priority_countries, input_html_options) #, priority_countries, options)
   end
 end
